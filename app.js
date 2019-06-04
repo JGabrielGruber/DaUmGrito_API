3e5850e62715cc31e056e281c80b817e760ffc3e
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-06-04 09:05:52 -0300

    Implementado mais origins para testes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({credentials: true, origin: 'http://localhost'}));
+app.use(cors({credentials: true, origin: ['http://localhost', 'http://localhost:8100']}));
 
 app.use('/', indexRouter);
 app.use('/api/clientes', clienteRouter);
